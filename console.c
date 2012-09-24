d44a95a1bf0d01636ce72a46b5c8a749e068a3fd
Jut Shanahan <jut.shanahan@gmail.com>
2012-09-24 01:45:22 -0700

    creatio ex materia

---
+++ console.c
@@ -0,0 +1,315 @@
+#ifndef FCY
+#define FCY 10000000		// 10.000000 MHz
+#endif
+#include "DSP28x_Project.h"     	   // Device Headerfile and Examples Include File, this include file includes all "F2806x_XXXXX" header files
+#include "console.h"
+#include "F2806x_Cla_typedefs.h"// F2806x CLA Type definitions
+#include <string.h>
+#include <stdlib.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_uart.h"
+#include "driverlib/debug.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/uart.h"
+#include "utils/cmdline.h"
+#include "utils/uartstdio.h"
+#include "uartcolors.h"
+
+
+#define MAX_COMMAND_LENGTH	32
+//#define DEBUG
+
+extern float Freq;   // frequency of timer0 interrupt in Hz
+
+
+void btoa(char *string, unsigned int var);      // convert 16bit variable into a string
+
+
+
+
+int Cmd_Encoder(int argc,char *argv[])
+{
+
+	float freq;
+	freq = atof(argv[2]);   // string to float
+	//EPwm1Regs.CMPB = (Uint16)(DC/100.0*((float)EPwm1Regs.TBPRD));
+	//EPwm1Regs.CMPA.half.CMPA = (Uint16)((1.0-DC/100.0)*((float)EPwm1Regs.TBPRD));
+	SysCtrlRegs.PCLKCR0.bit.TBCLKSYNC = 0;    // disable pwm clock
+
+
+	EPwm1Regs.TBPRD = 1000; // Period = 8000 TBCLK counts = 10kHz
+	EPwm1Regs.CMPA.half.CMPA = 250; // Compare A = 350 TBCLK counts
+	EPwm1Regs.CMPB = 500; // Compare B = 0 TBCLK counts
+	EPwm1Regs.TBPHS.all = 0; // Set Phase register to zero
+	EPwm1Regs.TBCTR = 0; // clear TB counter
+	EPwm1Regs.TBCTL.bit.CTRMODE = TB_COUNT_UPDOWN;
+	EPwm1Regs.TBCTL.bit.PHSEN = TB_DISABLE; // Phase loading disabled
+	EPwm1Regs.TBCTL.bit.PRDLD = TB_SHADOW;
+	EPwm1Regs.TBCTL.bit.SYNCOSEL = TB_SYNC_DISABLE;
+	EPwm1Regs.TBCTL.bit.HSPCLKDIV = TB_DIV1; // TBCLK = SYSCLK
+	EPwm1Regs.TBCTL.bit.CLKDIV = TB_DIV1;
+	EPwm1Regs.CMPCTL.bit.SHDWAMODE = CC_SHADOW;
+	EPwm1Regs.CMPCTL.bit.SHDWBMODE = CC_SHADOW;
+	EPwm1Regs.CMPCTL.bit.LOADAMODE = CC_CTR_ZERO; // load on CTR = Zero
+	EPwm1Regs.CMPCTL.bit.LOADBMODE = CC_CTR_ZERO; // load on CTR = Zero
+
+//	UARTprintf("argv[1]=%s\r\n",argv[1]);
+
+	if(strcmp(argv[1],"f")==0)
+	{
+		// A leads B
+		EPwm1Regs.AQCTLA.all = 0;
+		EPwm1Regs.AQCTLB.all = 0;
+		EPwm1Regs.AQCTLB.bit.ZRO = AQ_CLEAR;
+		EPwm1Regs.AQCTLB.bit.CBU = AQ_SET;
+		EPwm1Regs.AQCTLB.bit.CBD = AQ_CLEAR;
+		EPwm1Regs.AQCTLA.bit.ZRO = AQ_SET;
+		EPwm1Regs.AQCTLA.bit.PRD = AQ_CLEAR;
+		UARTprintf("direction = forward (A leads B)\r\n");
+	}
+	else if(strcmp(argv[1],"r")==0)
+	{
+		// B leads A
+		EPwm1Regs.AQCTLA.all = 0;
+		EPwm1Regs.AQCTLB.all = 0;
+		EPwm1Regs.AQCTLA.bit.ZRO = AQ_CLEAR;
+		EPwm1Regs.AQCTLA.bit.CBU = AQ_SET;
+		EPwm1Regs.AQCTLA.bit.CBD = AQ_CLEAR;
+		EPwm1Regs.AQCTLB.bit.ZRO = AQ_SET;
+		EPwm1Regs.AQCTLB.bit.PRD = AQ_CLEAR;
+		UARTprintf("direction = reverse (B leads A)\r\n");
+	}
+	else
+	{
+		UARTprintf(RED);
+		UARTprintf("invalid direction\r\n");
+		UARTprintf(restore);
+	}
+
+	SysCtrlRegs.PCLKCR0.bit.TBCLKSYNC = 1;    // enable pwm clock
+	UARTprintf("9\r\n");
+
+
+
+}
+
+
+int Cmd_LED_Freq(int argc,char *argv[])
+{
+
+	Freq = atof(argv[1]);     // convert first element in argument vector (array of strings) to a float using ascii to float function
+	if(Freq >= 0)
+	{
+		ConfigCpuTimer(&CpuTimer0, 80, 1.0/Freq*1000000.0);
+		CpuTimer0Regs.TCR.all = 0x4000; // Use write-only instruction to set TSS bit = 0 (starts timer) and TIE=1
+		UARTprintf("blink frequency set to %u Hz\r\n",(long)Freq);
+	}
+	else
+	{
+		UARTprintf("invalid frequency\r\n",(long)Freq);
+	}
+
+
+	return 0;
+}
+
+int Cmd_blink(int argc,char *argv[])
+{
+	GpioDataRegs.GPBTOGGLE.bit.GPIO34 = 1;
+	UARTprintf("Blink!!!\r\n");
+	return 0;
+}
+
+
+int Cmd_help(int argc, char *argv[])
+{
+    tCmdLineEntry *pEntry;
+
+    // Print some header text.
+//    printf("\a\033[31munknown register\033[37m\r\n");          //     \a=beep
+    UARTprintf("Available commands\n");
+    UARTprintf("------------------\n");
+    // Point at the beginning of the command table.
+    pEntry = &g_sCmdTable[0];
+
+    // Enter a loop to read each entry from the command table.  The end of the
+    // table has been reached when the command name is NULL.
+    while(pEntry->pcCmd)
+    {
+        // Print the command name and the brief description.
+        UARTprintf("%s%s\n", pEntry->pcCmd, pEntry->pcHelp);
+
+        // Advance to the next entry in the table.
+        pEntry++;
+    }
+
+    // Return success.
+    return(0);
+}
+
+int Cmd_Get_Reg(int argc, char *argv[])
+{
+	char string[16]={0};
+	volatile unsigned long *ptr, x, y;
+
+	x = (unsigned long)(strtol(argv[1], 0, 16));   // "strtol" converts the string into a long integer
+	UARTprintf("address requested = 0x%x\r\n",x);
+	ptr = (unsigned long *)x;
+	y = *ptr;
+	btoa(string,y);
+	UARTprintf("%s.b = %u = 0x%x\r\n",string,(Uint32)((Uint16)y),(Uint32)((Uint16)y));
+//	UARTprintf("partid = 0x%x\r\n",*(unsigned long *)(0x883));
+
+
+	return 1;
+
+}
+
+
+int Cmd_Set_Reg(int argc, char *argv[])
+{
+	char string[16]={0};
+	volatile unsigned long *ptr, addr, reg, y;
+	volatile unsigned int *ptr16;
+
+	addr = (unsigned long)(strtol(argv[1], 0, 16));   // "strtol" converts the string into a long integer, assumes string represents a hex value
+	reg = (unsigned long)(strtol(argv[2], 0, 16));   // "strtol" converts the string into a long integer, assumes string represents a hex value
+
+	ptr = (unsigned long *)addr;
+	ptr16 = (unsigned int *)addr;
+	UARTprintf("command request: set 0x%x to 0x%x\r\n",(Uint32)ptr16, reg);
+	*ptr16 = (Uint16)reg;
+	UARTprintf("0x%x = 0x%x\r\n",(Uint32)ptr16, (Uint32)(*ptr16));
+
+
+}
+
+
+
+
+char get_command(char *command, int maxlength);
+//char gr(char *);
+//char sr(char *);
+
+
+
+char misc(char *arg)
+{
+//	printf("doing misc things\r\n");
+
+	return 1;
+	
+}
+
+
+void dispatch_console(void)
+{
+	static char command[MAX_COMMAND_LENGTH] = {0};		// MAX_COMMAND_LENGTH = 32
+	static char lastcmd[MAX_COMMAND_LENGTH] = {0};		// MAX_COMMAND_LENGTH = 32
+	static char init = 1;
+	int length = strlen(command);
+	//printf("length = %d\r\n",length);
+	
+	if(init == 0)
+	{
+		CmdLineProcess("help");
+		UARTprintf("> ");
+		init = 1;
+	}
+	
+	if(get_command(command+length, MAX_COMMAND_LENGTH-length) == 0)
+	{
+//		strcpy(lastcmd,command);
+		UARTprintf("  %s  (command received)\r\n",command);
+		CmdLineProcess(command);
+		memset(command, 0, sizeof(command));
+		UARTprintf("> ");
+	}
+}
+
+char get_command(char *command, int maxlength)
+{
+	char input=0;
+	long length=0;
+
+	if(UARTCharsAvail(UART0_BASE) == 0)
+	{
+		//UARTprintf("no chars available :( \r\n");
+		return 1;
+	}
+	else
+	{
+		input = UARTCharGetNonBlocking(UART0_BASE);
+//		UARTprintf("length=%u\r\n",length);
+	}
+
+	
+	while(input != '\r' && input != '\n')
+	{
+		if(input == 0x7F)  // backspace
+		{
+			//U1TXREG = input;
+			UARTCharPutNonBlocking(UART0_BASE, (unsigned char)(input));
+			//UARTprintf("backspace");
+			//printf("%c", input);
+			command--;
+			*command = 0;			// set previous input char = NULL character
+			command++;
+			return 1;
+		}
+		
+		
+		*command = input;
+		//UARTprintf("%c", *command++);
+		UARTCharPutNonBlocking(UART0_BASE, *command++);
+		if(++length == maxlength - 1)
+			break;
+		
+		if(UARTCharsAvail(UART0_BASE)==0)
+			return 1;
+		else
+			input=UARTCharGetNonBlocking(UART0_BASE);
+	
+	}
+	UARTprintf("\n");
+	
+	return 0;
+}
+
+
+// convert 16bit variable into a string
+void btoa(char *string, unsigned int var)
+{
+
+	unsigned int x=0, i=0;
+
+	// initialize array to all
+	for(i=0; i<16; i++)
+		string[i] = 48;				// Decimal 48 is equivalent to the ascii char "0"
+
+	string[16] = 0;					// the last char in the char array must be a 0
+
+	for(i=0; i<17; i++)
+	{
+		x = var << i;				// shift var to the left
+		x = x & 0x8000;				// zero out bits 0 - 14, leave bit 15 intact
+		if(x == 0x8000)				// is bit 15 set?
+			string[i] = 49;			// Decimal 49 is equivalent to the ascii char "1"
+
+		x = 0;
+	}
+
+}
+
+tCmdLineEntry g_sCmdTable[] =
+{
+    { "help",   Cmd_help,      " : Display list of commands" },
+    { "blink",  Cmd_blink,     " : Blink the LED"},
+    { "ledf",  Cmd_LED_Freq,   " : Set LED blinking frequency"},
+    { "gr",    Cmd_Get_Reg,    " : Get Register"},
+    { "sr",    Cmd_Set_Reg,    " : Set Register"},
+    { "enc",    Cmd_Encoder,    " : Set encoder frequency:  enc {f | r} <freq> <ticks per revolution>"},
+    { 0, 0, 0 }
+};
---
+++ encoder.c
@@ -0,0 +1,218 @@
+// JPMC
+// Jut Shanahan, Paul Karplus Motor Controller
+// Copyright: 2012
+//###########################################################################
+// Description: Piccolo Control Stick with F28069 Processor
+//
+// Rev		Date		Edited By		Revision Notes
+// 0.01		8/15/2012	Jut				Initial rev. Basic serial command functionality working. Delete, printf, and several other things implemented.
+// 0.02		8/15/2012	PK				Moved dispatch_console command to the SCIA rx interrupt. Added a command line function that blinks the LED on the controlStick. Cleaned up and commented code.
+// 0.03		8/16/2012	Jut				Added command to set led blink frequency
+// 0.04		8/18/2012	Jut				Added command to get value at any data memory location.  Added UART color defines.
+//###########################################################################
+#define REV "0.04"
+
+#include "DSP28x_Project.h"     	   // Device Headerfile and Examples Include File
+#include <string.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
+#include "driverlib/uart.h"
+#include "utils/cmdline.h"
+#include "console.h"
+#include "utils/uartstdio.h"
+#include "uartcolors.h"
+
+
+// Prototype statements for functions found within this file.
+__interrupt void cpu_timer0_isr(void);
+__interrupt void cpu_timer1_isr(void);
+__interrupt void cpu_timer2_isr(void);
+__interrupt void scia_rx_isr(void);					// sci receive interrupt function
+void init(void);
+
+// Initialize global variables for this file
+float Freq = 1;   // frequency of timer0 interrupt in Hz
+
+
+void main(void)
+{
+	init();			// Sets up the sci and GPIO registers
+
+	// Print bootup message containing device title and revision number
+	UARTprintf("\r\n");
+	UARTprintf(BOLDCYAN);
+	UARTprintf("JSPK Motor Controller\r\n");
+	UARTprintf("Rev %s\r\n",REV);
+	UARTprintf(restore);
+
+//	UARTprintf("\033[36mJSPK Motor Controller\033[37m\r\n");
+//	UARTprintf("\033[36mRev %s\033[37m\r\n",REV);
+	CmdLineProcess("help");
+	UARTprintf("> ");
+
+	// Infinite loop.
+   while(1){}
+}
+
+// Initial setup of all registers
+void init(void)
+{
+	// Initialize System Control:
+	// PLL, WatchDog, enable Peripheral Clocks
+	InitSysCtrl();
+
+	// Initialize Sci GPIO
+	InitSciaGpio();
+
+	// init pwn
+	EALLOW;
+
+	/* Disable internal pull-up for the selected output pins
+	for reduced power consumption */
+	// Pull-ups can be enabled or disabled by the user.
+	// Comment out other unwanted lines.
+
+	GpioCtrlRegs.GPAPUD.bit.GPIO0 = 1;    // Disable pull-up on GPIO0 (EPWM1A)
+	GpioCtrlRegs.GPAPUD.bit.GPIO1 = 1;    // Disable pull-up on GPIO1 (EPWM1B)
+
+	/* Configure EPWM-1 pins using GPIO regs*/
+	// This specifies which of the possible GPIO pins will be EPWM1 functional pins.
+	// Comment out other unwanted lines.
+
+	GpioCtrlRegs.GPAMUX1.bit.GPIO0 = 1;   // Configure GPIO0 as EPWM1A
+	GpioCtrlRegs.GPAMUX1.bit.GPIO1 = 1;   // Configure GPIO1 as EPWM1B
+
+	EDIS;
+
+	// Clear all interrupts and initialize PIE vector table. Disable CPU interrupts
+	DINT;
+
+	// Disable CPU interrupts and clear all CPU interrupt flags:
+	IER = 0x0000;
+	IFR = 0x0000;
+
+	// Initialize the PIE control registers to their default state.
+	// The default state is all PIE interrupts disabled and flags
+	// are cleared.
+	InitPieCtrl();
+
+	// Initialize the PIE vector table with pointers to the shell Interrupt
+	// Service Routines (ISR).
+	// This will populate the entire table, even if the interrupt
+	// is not used in this example.  This is useful for debug purposes.
+	// The shell ISR routines are found in F2806x_DefaultIsr.c.
+	// This function is found in F2806x_PieVect.c.
+	InitPieVectTable();
+
+	// Interrupts that are used in this file are re-mapped to
+	// ISR functions found within this file.
+	EALLOW;  // This is needed to write to EALLOW protected registers
+	PieVectTable.TINT0 = &cpu_timer0_isr;
+	PieVectTable.TINT1 = &cpu_timer1_isr;
+	PieVectTable.TINT2 = &cpu_timer2_isr;
+	PieVectTable.SCIRXINTA = &scia_rx_isr;		// SciA rx interrupt
+	EDIS;    // This is needed to disable write to EALLOW protected registers
+
+	// Step 4. Initialize the Device Peripheral.
+	InitCpuTimers();   // For this file, only initialize the Cpu Timers
+
+	// Configure CPU-Timer 0, 1, and 2 to interrupt every second:
+	// 80MHz CPU Freq, 1 second Period (in uSeconds)
+	ConfigCpuTimer(&CpuTimer0, 80, 1.0/Freq*1000000.0);
+	ConfigCpuTimer(&CpuTimer1, 80, 1000000);
+	ConfigCpuTimer(&CpuTimer2, 80, 1000000);
+
+	// To ensure precise timing, use write-only instructions to write to the entire register. Therefore, if any
+	// of the configuration bits are changed in ConfigCpuTimer and InitCpuTimers (in F2806x_CpuTimers.h), the
+	// below settings must also be updated.
+	CpuTimer0Regs.TCR.all = 0x4000; // Use write-only instruction to set TSS bit = 0 (starts timer) and TIE=1
+	CpuTimer1Regs.TCR.all = 0x4000; // Use write-only instruction to set TSS bit = 0 (starts timer) and TIE=1
+	CpuTimer2Regs.TCR.all = 0x4000; // Use write-only instruction to set TSS bit = 0 (starts timer) and TIE=1
+
+	// Enable CPU int1 which is connected to CPU-Timer 0, CPU int 9 which is connected to SCIA_rx and SCIA_tx, CPU int13
+	// which is connected to CPU-Timer 1, and CPU int 14, which is connected
+	// to CPU-Timer 2:
+	IER |= M_INT1;    // enable group 1 interrupts
+	IER |= M_INT9;    // enable group 9 interrupts (scia rx and tx)
+	IER |= M_INT13;   // enable group 13 interrupts
+	IER |= M_INT14;   // enable group 14 interrupts
+
+	// Enable individual PIE Table interrupts
+	PieCtrlRegs.PIEIER1.bit.INTx7 = 1;		// Enable TINT0 in the PIE: Group 1 interrupt 7
+    PieCtrlRegs.PIEIER9.bit.INTx1 = 1;		// Enable SCIRXINTA in the PIE: Group 9 interrupt 1
+
+
+	// Enable global Interrupts and higher priority real-time debug events:
+	EINT;   // Enable Global interrupt INTM
+	ERTM;   // Enable Global realtime interrupt DBGM
+
+	// Intialize UART(INT_SCIRXINTA, UARTStdioIntHandler);
+	UARTStdioInitExpClk(0,9600);
+
+	// Enable Sci tx and rx interupts within Sci control register
+	SciaRegs.SCICTL2.bit.TXINTENA = 1;
+	SciaRegs.SCICTL2.bit.RXBKINTENA = 1;
+
+	// Setup LED on GPIO-34 for Sci blink command. LED is toggled by blink command is sent.
+	EALLOW;
+	GpioCtrlRegs.GPBMUX1.bit.GPIO34 = 0;	// 0=GPIO,  1=COMP2OUT,  2=EMU1,  3=Resv
+	GpioCtrlRegs.GPBDIR.bit.GPIO34 = 1;		// 1=OUTput,  0=INput
+	GpioDataRegs.GPBCLEAR.bit.GPIO34 = 1;	// uncomment if --> Set Low initially
+	//GpioDataRegs.GPBSET.bit.GPIO34 = 1;	// uncomment if --> Set High initially
+	EDIS;
+}
+
+// Sci rx interrupt function.
+__interrupt void scia_rx_isr(void)
+{
+	dispatch_console();							// Processes the received data.
+	PieCtrlRegs.PIEACK.all = PIEACK_GROUP9;		// Acknowledges the PIE group so that another interrupt can occur
+	//SciaRegs.SCIFFRX.bit.RXFFINTCLR = 1;		// (PK) I tried commenting this out and it did not affect performance. So it must not be necessary.
+	//SciaRegs.SCIFFRX.bit.RXFFOVRCLR = 1;		// (PK) I tried commenting this out and it did not affect performance. So it must not be necessary.
+}
+__interrupt void cpu_timer0_isr(void)
+{
+   CpuTimer0.InterruptCount++;
+//   UARTprintf("cpu_timer0_isr %u\r\n",CpuTimer0.InterruptCount);
+   GpioDataRegs.GPBTOGGLE.bit.GPIO34 = 1;
+
+
+
+//   if(SciaRegs.SCIFFRX.bit.RXFFINT)
+//	   UARTprintf("SciaRegs.SCIFFRX.bit.RXFFINT = 1\r\n");
+//   else
+//	   UARTprintf("SciaRegs.SCIFFRX.bit.RXFFINT = 0\r\n");
+//
+//
+//   if(PieCtrlRegs.PIEIFR9.bit.INTx1)
+//	   UARTprintf("PieCtrlRegs.PIEIFR9.bit.INTx1 = 1\r\n");
+//   else
+//	   UARTprintf("PieCtrlRegs.PIEIFR9.bit.INTx1 = 0\r\n");
+
+
+   // Acknowledge this interrupt to receive more interrupts from group 1
+   PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
+}
+
+__interrupt void cpu_timer1_isr(void)
+{
+   CpuTimer1.InterruptCount++;
+   // The CPU acknowledges the interrupt.
+   EDIS;
+}
+
+__interrupt void cpu_timer2_isr(void)
+{  EALLOW;
+   CpuTimer2.InterruptCount++;
+   // The CPU acknowledges the interrupt.
+   EDIS;
+}
+
+
+//===========================================================================
+//
+//===========================================================================
